fffe514dd5f98c43c8188fe44028c5775e332c62
Elliott Judd <e@juddus.com>
2018-10-31 00:41:23 +0000

    add all main buttons

--- src/app/components/button_area.ts
+++ src/app/components/button_area.ts
@@ -7,13 +7,26 @@ import * as Buttons from './buttons';
 const buttonArea = () => {
     return div({ id: 'button-area' }, [
         section([
-            Buttons.ttsButton,
-            Buttons.playButton,
-            Buttons.pauseButton,
-            Buttons.stopButton,
+            div({ className: 'group' }, [
+                Buttons.playButton(),
+                Buttons.pauseButton(),
+                Buttons.stopButton(),
+            ]),
+            div({ className: 'group' }, [
+                Buttons.ttsButton(),
+                Buttons.incButton(),
+                Buttons.decButton(),
+                Buttons.textOpsButton(),
+                Buttons.magButton(),
+                Buttons.maskButton(),
+                Buttons.rulerButton(),
+                Buttons.srButton(),
+            ]),
         ]),
         section([
-            Buttons.closeButton,
+            Buttons.resetButton(),
+            Buttons.settingsButton(),
+            Buttons.closeButton(),
         ]),
     ]);
 };
--- src/app/components/buttons.ts
+++ src/app/components/buttons.ts
@@ -13,8 +13,10 @@ const closeButton = () => {
                 tippy('#accessabar #close', {
                     arrow: true,
                     content: 'Close Accessabar',
+                    placement: 'bottom',
                 });
             },
+            tabIndex: 0,
         },
         [
             i({
@@ -34,8 +36,10 @@ const ttsButton = () => {
                 tippy('#accessabar #tts', {
                     arrow: true,
                     content: 'Text to Speech',
+                    placement: 'bottom',
                 });
             },
+            tabIndex: 0,
         },
         [
             i({
@@ -55,8 +59,10 @@ const playButton = () => {
                 tippy('#accessabar #play', {
                     arrow: true,
                     content: 'Play',
+                    placement: 'bottom',
                 });
             },
+            tabIndex: 0,
         },
         [
             i({
@@ -76,8 +82,10 @@ const pauseButton = () => {
                 tippy('#accessabar #pause', {
                     arrow: true,
                     content: 'Pause',
+                    placement: 'bottom',
                 });
             },
+            tabIndex: 0,
         },
         [
             i({
@@ -97,13 +105,222 @@ const stopButton = () => {
                 tippy('#accessabar #stop', {
                     arrow: true,
                     content: 'Stop',
+                    placement: 'bottom',
                 });
             },
+            tabIndex: 0,
         },
         [
             i({
                 'aria-hidden': true,
                 className: 'ab-icon-stop',
+            }),
+        ],
+    );
+};
+
+const incButton = () => {
+    return button(
+        {
+            'aria-label': 'Increase font size',
+            id: 'font-increase',
+            oncreate: () => {
+                tippy('#accessabar #font-increase', {
+                    arrow: true,
+                    content: 'Increase font size',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-plus',
+            }),
+        ],
+    );
+};
+
+const decButton = () => {
+    return button(
+        {
+            'aria-label': 'Decrease font size',
+            id: 'font-decrease',
+            oncreate: () => {
+                tippy('#accessabar #font-decrease', {
+                    arrow: true,
+                    content: 'Decrease font size',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-minus',
+            }),
+        ],
+    );
+};
+
+const textOpsButton = () => {
+    return button(
+        {
+            'aria-label': 'Text options',
+            id: 'text-options',
+            oncreate: () => {
+                tippy('#accessabar #text-options', {
+                    arrow: true,
+                    content: 'Text Options',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-font',
+            }),
+        ],
+    );
+};
+
+const magButton = () => {
+    return button(
+        {
+            'aria-label': 'Magnifier',
+            id: 'magnifier',
+            oncreate: () => {
+                tippy('#accessabar #magnifier', {
+                    arrow: true,
+                    content: 'Magnifier',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-zoom-in',
+            }),
+        ],
+    );
+};
+
+const maskButton = () => {
+    return button(
+        {
+            'aria-label': 'Screen Masking',
+            id: 'screen-mask',
+            oncreate: () => {
+                tippy('#accessabar #screen-mask', {
+                    arrow: true,
+                    content: 'Screen Masking',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-brush',
+            }),
+        ],
+    );
+};
+
+const rulerButton = () => {
+    return button(
+        {
+            'aria-label': 'Reading rulers',
+            id: 'rulers',
+            oncreate: () => {
+                tippy('#accessabar #rulers', {
+                    arrow: true,
+                    content: 'Reading Rulers',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-ruler',
+            }),
+        ],
+    );
+};
+
+const srButton = () => {
+    return button(
+        {
+            'aria-label': 'Speech recognition',
+            id: 'speech-recognition',
+            oncreate: () => {
+                tippy('#accessabar #speech-recognition', {
+                    arrow: true,
+                    content: 'Speech Recognition',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-mic',
+            }),
+        ],
+    );
+};
+
+const resetButton = () => {
+    return button(
+        {
+            'aria-label': 'Reset accessabar',
+            id: 'reset',
+            oncreate: () => {
+                tippy('#accessabar #reset', {
+                    arrow: true,
+                    content: 'Reset',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-reset',
+            }),
+        ],
+    );
+};
+
+const settingsButton = () => {
+    return button(
+        {
+            'aria-label': 'Settings',
+            id: 'settings',
+            oncreate: () => {
+                tippy('#accessabar #settings', {
+                    arrow: true,
+                    content: 'Settings',
+                    placement: 'bottom',
+                });
+            },
+            tabIndex: 0,
+        },
+        [
+            i({
+                'aria-hidden': true,
+                className: 'ab-icon-cog',
             }),
         ],
     );
@@ -120,4 +337,13 @@ export {
     playButton,
     pauseButton,
     stopButton,
+    incButton,
+    decButton,
+    textOpsButton,
+    magButton,
+    maskButton,
+    rulerButton,
+    srButton,
+    resetButton,
+    settingsButton,
 };
