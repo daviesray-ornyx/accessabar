d729c216e07d1d5e1561c1b89e5945f877e7b851
Elliott Judd <e@juddus.com>
2018-11-15 19:57:07 +0000

    add growable; add menu area; pass both state and actions

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,7 @@ import { View } from 'hyperapp';
 import { div } from '@hyperapp/html';
 import buttonArea from './components/button_area.component';
 import { hideButton } from './components/buttons.component';
+import menuArea from './components/menu_area.component';
 
 /**
  * Main container for all Accessabar elements
@@ -9,9 +10,10 @@ import { hideButton } from './components/buttons.component';
  * @param {*} state
  * @param {*} actions
  */
-const mainView: View<Accessabar.IState, Accessabar.IActions> = (state, actions) => div({ class: 'accessabar-container' }, [
+const mainView: View<Accessabar.IState, Accessabar.IActions> = (state, actions) => div({ class: 'accessabar-container growable' }, [
     hideButton(state, actions),
-    buttonArea(actions),
+    buttonArea(state, actions),
+    menuArea(state, actions),
 ]);
 
 export default mainView;
