128adb48763da2b49bcbf9e50c45efc63ce065ff
Elliott Judd <e@juddus.com>
2018-11-20 15:31:48 +0000

    Add new menu types

--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -3,6 +3,12 @@
 declare namespace Accessabar {
     interface IState {
         abarHidden: boolean;
+        menuCanDrag: boolean;
+        menuPosX: number | boolean;
+        menuPosY: number | boolean;
+        menuMouseX: number;
+        menuMouseY: number;
+        menuEvent: boolean;
     }
 
     interface IActions extends
@@ -10,7 +16,8 @@ declare namespace Accessabar {
         IUtilActions,
         IFontActions,
         IResetActions,
-        ICloseActions
+        ICloseActions,
+        IMenuActions
     {}
 
     interface IHideActions {
@@ -33,6 +40,15 @@ declare namespace Accessabar {
 
     interface ICloseActions {
         closeAccessabar(): (state: Accessabar.IState, actions: Accessabar.IActions) => Accessabar.IState;
+    }
+
+    interface IMenuActions {
+        addMenuListener(): (state: Accessabar.IState, actions: Accessabar.IActions) => Accessabar.IState;
+        moveMenu(event: Event): (state: Accessabar.IState) => Accessabar.IState;
+        updatePosition(el: HTMLElement): Accessabar.IState;
+        updateMousePosition(event: MouseEvent): Accessabar.IState;
+        startDrag(event: MouseEvent): (state: Accessabar.IState, actions: Accessabar.IActions) => Accessabar.IState;
+        stopDrag(): Accessabar.IState;
     }
 
     interface IConfigObject {
