5546bd3cddda6df7b50366310fcaa5e53a081ebf
Elliott Judd <e@juddus.com>
2018-11-20 15:31:27 +0000

    remove comment

--- src/app/actions/close.actions.ts
+++ src/app/actions/close.actions.ts
@@ -1,7 +1,6 @@
 import { ActionsType } from 'hyperapp';
 
 const closeActions: ActionsType<Accessabar.IState, Accessabar.ICloseActions> = {
-    // TODO: figure out odd typing
     closeAccessabar: () => ({ abarHidden }, { resetAll }: Accessabar.IActions) => {
         resetAll();
         window.abar.close();
