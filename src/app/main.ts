616fe408105f52f8fe3a87acce23f2349dffe6d1
Elliott Judd <e@juddus.com>
2018-11-29 19:17:33 +0000

    change layout of bar components

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,16 +4,32 @@ import buttonArea from './components/button_area.component';
 import { hideButton } from './components/buttons.component';
 import menuArea from './components/menu_area.component';
 
+const innerBar = (state, actions) => {
+    return div({ class: 'bar growable' }, [
+        div({ class: 'bar-container growable' }, [
+            buttonArea(state, actions),
+        ]),
+    ]);
+};
+
+const underBar = (state, actions) => {
+    return div({ class: 'underbar growable' }, [
+        hideButton(state, actions),
+    ]);
+};
+
 /**
  * Main container for all Accessabar elements
  *
  * @param {*} state
  * @param {*} actions
  */
-const mainView: View<Accessabar.IState, Accessabar.IActions> = (state, actions) => div({ class: 'accessabar-container growable' }, [
-    hideButton(state, actions),
-    buttonArea(state, actions),
-    menuArea(state, actions),
-]);
+const mainView: View<Accessabar.IState, Accessabar.IActions> = (state, actions) => {
+    return div({ class: 'bar-grid' }, [
+        innerBar(state, actions),
+        underBar(state, actions),
+        menuArea(state, actions),
+    ]);
+};
 
 export default mainView;
