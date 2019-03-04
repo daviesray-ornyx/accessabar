5968bd4c2a16d81cf3a01ac8cf3e3c122611df1c
Elliott Judd <e@juddus.com>
2019-03-04 17:57:11 +0000

    Change regex to match better

--- src/app/actions/mag.actions.ts
+++ src/app/actions/mag.actions.ts
@@ -1,11 +1,13 @@
 import { ActionsType } from 'hyperapp';
 // import fromEvent from 'rxjs';
 
+interface IDragEvent extends MouseEvent, TouchEvent {}
+
 const magActions: ActionsType<Accessabar.IState, Accessabar.IMagActions> = {
     magEnable: () => ({ magActive }) => {
         let pageContent = document.documentElement.outerHTML;
-        const abarEl = /<accessabar-app.+<\/accessabar-app>/;
-        const abarScripts = /<script.+src=\\".+accessabar.+<\/script>/;
+        const abarEl = /<accessabar-app.*<\/accessabar-app>/;
+        const abarScripts = /<script.*src=.*accessabar.*<\/script>/;
 
         pageContent = pageContent.replace(abarEl, '');
         pageContent = pageContent.replace(abarScripts, '');
