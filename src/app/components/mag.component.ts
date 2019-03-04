f6d9d6b4bc2943a0abf02214379a6ca6ab0cf064
Elliott Judd <e@juddus.com>
2019-03-04 03:06:22 +0000

    Add magnifier window

--- src/app/components/mag.component.ts
+++ src/app/components/mag.component.ts
@@ -1,9 +1,14 @@
 import {
     div,
+    i,
 } from '@hyperapp/html';
 
 const mag = () => {
-    return div({ id: 'ab-magnifier', class: 'ab-magnifier ab-draggable ab-hide' });
+    return div({ id: 'ab-magnifier', class: 'ab-magnifier ab-draggable' }, [
+        div({ class: 'ab-drag-circle' }, [
+            i({ class: 'ab-icon ab-icon-move' }),
+        ]),
+    ]);
 };
 
 export default mag;
