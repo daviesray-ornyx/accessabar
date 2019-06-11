fc2c87c63423ea9c1d6f6ddc1ae0ae3e38732fdb
Elliott Judd <e@juddus.com>
2019-06-11 19:23:56 +0100

    Add ruler component

---
+++ src/app/components/ruler.component.ts
@@ -0,0 +1,10 @@
+import {
+    div,
+} from '@hyperapp/html';
+
+const rulerReadingBar = (state) => {
+    return div({ class: 'reading-ruler' });
+};
+
+export default rulerReadingBar;
+export { rulerReadingBar };
