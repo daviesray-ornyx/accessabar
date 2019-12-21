c05a9379a15e802587f46d6481f4c3bc5a0753bc
juddus <e@juddus.com>
2019-12-21 11:38:11 +0000

    split header into separate variable

--- src/app/components/settings.component.ts
+++ src/app/components/settings.component.ts
@@ -1,31 +1,35 @@
 import { h } from 'hyperapp';
 
+const settingsHeader = ({ settingsHidden }, { settingsClose }) => {
+    return h('ab-settings-header', { class: 'ab-settings-header' }, [
+        h('ab-logo', { class: 'ab-logo-large ab-settings-logo', 'aria-label': 'Accessabar logo' }, [
+            h('ab-logo-img', { class: 'ab-logo-img', alt: 'Accessabar Logo' }),
+        ]),
+        h(
+            'ab-settings-close-button',
+            {
+                'aria-controls': 'ab-settings',
+                'aria-label': 'Close Settings',
+                'aria-pressed': String(settingsHidden),
+                'aria-role': 'button',
+                class: 'ab-settings-close-button',
+                onclick: () => {
+                    settingsClose();
+                },
+            },
+            [
+                h('ab-icon', {
+                    'aria-hidden': 'true',
+                    class: 'ab-icon ab-icon-cross',
+                }),
+            ],
+        ),
+    ]);
+};
+
 const settingsMenu = (state, actions) => {
     return h('ab-settings-menu', { id: 'ab-settings', class: `ab-settings ${state.settingsHidden ? 'ab-hide' : ''}`, 'aria-label': 'Accessabar settings' }, [
-        h('ab-settings-header', { class: 'ab-settings-header' }, [
-            h('ab-logo', { class: 'ab-logo-large ab-settings-logo', 'aria-label': 'Accessabar logo' }, [
-                h('ab-logo-img', { class: 'ab-logo-img', alt: 'Accessabar Logo' }),
-            ]),
-            h(
-                'ab-settings-close-button',
-                {
-                    'aria-controls': 'ab-settings',
-                    'aria-label': 'Close Settings',
-                    'aria-pressed': String(state.settingsHidden),
-                    'aria-role': 'button',
-                    class: 'ab-settings-close-button',
-                    onclick: () => {
-                        actions.settingsClose();
-                    },
-                },
-                [
-                    h('ab-icon', {
-                        'aria-hidden': 'true',
-                        class: 'ab-icon ab-icon-cross',
-                    }),
-                ],
-            ),
-        ]),
+        settingsHeader(state, actions),
     ]);
 };
 
