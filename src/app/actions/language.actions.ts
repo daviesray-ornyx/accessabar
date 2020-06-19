fc385a60c64aa569cffe66436862ebe8407e0a4f
juddus <github@juddus.com>
2020-06-19 08:07:07 +0100

    update language to v2

--- src/app/actions/language.actions.ts
+++ src/app/actions/language.actions.ts
@@ -1,92 +1,27 @@
-import { ActionsType } from 'hyperapp';
 import languageConfig from '../../config/language.config.json5';
+import {getParents} from './ace.actions';
+import {apiGetTranslation} from './api.actions';
 
-/**
- * Fetches and returns parents of text nodes in the document
- *
- * @returns {Set<HTMLElement>}
- */
-function getParents(): Set<HTMLElement> {
-    const elements = document.body.childNodes;
-    const taggedElements: ChildNode[] = [];
-    const textNodes: Node[] = [];
-    const parentElements: Set<HTMLElement> = new Set();
+function languageChangeAll(state: Ace.State, key?: string) {
+  const {languageCurrentKey} = state;
+  const currentKey: string = key || languageCurrentKey;
+  if (currentKey.length <= 0) {
+    return;
+  }
 
-    // Filter through immediate child elements of body
-    // and get elements to walk
-    for (const el of elements) {
-        // Do not add accessabar or elements with no children
-        if (el !== window.abar.mainElement && el.childNodes.length !== 0) {
-            taggedElements.push(el);
-        }
-    }
+  const currentLanguageCode = languageConfig[currentKey].code || 'en';
+  const parentElements = getParents();
 
-    for (const el of taggedElements) {
-        // Will find all text nodes in element
-        const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+  parentElements.forEach(async element => {
+    const elementTextContent = element.textContent;
 
-        // Push each text node found into array
-        while (walker.nextNode()) {
-            textNodes.push(walker.currentNode);
-        }
-    }
+    const req = await apiGetTranslation({
+      strings: [elementTextContent],
+      to: currentLanguageCode,
+    });
 
-    for (const node of textNodes) {
-        const text = (node.textContent || '').trim();
-
-        // Do not add empty text nodes or line feeds in HTML
-        if (text === '' || text === '\n') {
-            continue;
-        }
-
-        const parent = node.parentElement;
-
-        if (!parent) {
-            continue;
-        }
-
-        // Do not add tippy tooltips
-        if (parent.classList.contains('tippy-content')) {
-            continue;
-        }
-
-        // Do not add duplicates
-        if (parentElements.has(parent)) {
-            continue;
-        }
-
-        parentElements.add(parent);
-    }
-
-    return parentElements;
+    element.textContent = req?.trans[0] || elementTextContent;
+  });
 }
 
-const languageActions: ActionsType<Accessabar.IState, Accessabar.ILanguageActions> = {
-    /**
-     * Translates all text on the page to a specified language
-     *
-     */
-    languageChangeAll: (key?: string) => ({ languageCurrentKey }, { apiGetTranslation }: Accessabar.IActions) => {
-        const currentKey: string = key || languageCurrentKey;
-        if (currentKey.length <= 0) {
-            return;
-        }
-
-        const currentLanguageCode = languageConfig[currentKey].code || 'en';
-        const parentElements = getParents();
-
-        parentElements.forEach(async (element) => {
-            let elementTextContent = element.textContent;
-
-            const req = await apiGetTranslation({
-                strings: [elementTextContent],
-                to: currentLanguageCode,
-            });
-
-            element.textContent = req?.trans[0] || elementTextContent;
-        });
-    },
-};
-
-export default languageActions;
-export { languageActions };
+export {languageChangeAll};
