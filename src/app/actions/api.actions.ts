c536f607bef42b03b81c905fc131b63109335775
juddus <github@juddus.com>
2020-06-19 02:42:53 +0100

    update api to v2

--- src/app/actions/api.actions.ts
+++ src/app/actions/api.actions.ts
@@ -1,69 +1,65 @@
-import { ActionsType } from 'hyperapp';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 async function doRequest(link, req) {
-    return fetch(link, req).catch((err) => {
-        console.log('[ACE API] Error: ', err);
-    });
+  return fetch(link, req).catch(err => {
+    console.log('[ACE API] Error: ', err);
+  });
 }
 
 async function handleRequest(link, req) {
-    const res = await doRequest(link, req);
-    if (res) {
-        return res.json();
-    }
+  const res = await doRequest(link, req);
+  if (res) {
+    return res.json();
+  }
 }
 
-const apiActions: ActionsType<Accessabar.IState, Accessabar.IApiActions> = {
-    apiSendEvent: async (eventType: string) => {
-        if (!localStorage) {
-            return;
-        }
+async function apiSendEvent(eventType: string) {
+  if (!localStorage) {
+    return;
+  }
 
-        const host = location.hostname;
+  const host = location.hostname;
 
-        if (host === 'localhost' || host === '') {
-            return;
-        }
+  if (host === 'localhost' || host === '') {
+    return;
+  }
 
-        let userID = localStorage.getItem('ace_user');
+  let userID = localStorage.getItem('ace_user');
 
-        if (!userID) {
-            const newID = uuidv4();
-            localStorage.setItem('ace_user', newID);
+  if (!userID) {
+    const newID = uuidv4();
+    localStorage.setItem('ace_user', newID);
 
-            userID = newID;
-        }
+    userID = newID;
+  }
 
-        const data = {
-            host,
-            user: userID,
-            type: eventType.trim(),
-        };
+  const data = {
+    host,
+    user: userID,
+    type: eventType.trim(),
+  };
 
-        const req = {
-            body: JSON.stringify(data),
-            method: 'POST',
-            mode: 'cors',
-            redirect: 'follow',
-            headers: { 'Content-Type': 'application/json' },
-        };
+  const req = {
+    body: JSON.stringify(data),
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
+    headers: {'Content-Type': 'application/json'},
+  };
 
-        await handleRequest('https://ace-ctrl.acetoolbar.com/api/v1/event/add', req);
-    },
+  await handleRequest('https://ace-ctrl.acetoolbar.com/api/v1/event/add', req);
+}
 
-    apiGetTranslation: async (data: { [x: string]: any }) => {
-        const req = {
-            body: JSON.stringify(data),
-            method: 'POST',
-            mode: 'cors',
-            redirect: 'follow',
-            headers: { 'Content-Type': 'application/json' },
-        };
+async function apiGetTranslation(data: {[x: string]: any}) {
+  const req = {
+    body: JSON.stringify(data),
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
+    headers: {'Content-Type': 'application/json'},
+  };
 
-        return handleRequest('https://trans.acetoolbar.com/api/v1/translate', req);
-    },
-};
+  return handleRequest('https://trans.acetoolbar.com/api/v1/translate', req);
+}
 
-export default apiActions;
-export { apiActions };
+export {apiSendEvent, apiGetTranslation};
