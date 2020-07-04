b91df1622a09ff22facc2cc56222e3fcc2ea31f1
juddus <github@juddus.com>
2020-07-04 16:33:59 +0100

    remove depreciated configs

--- ava.config.js
+++ ava.config.js
@@ -1,8 +1,6 @@
 const config = {
   files: ['tests/ava/**/*.ts'],
-  sources: ['src/**/*.ts'],
   extensions: ['ts'],
-  compileEnhancements: true,
   cache: true,
   require: ['ts-node/register'],
 };
