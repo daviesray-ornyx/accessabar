fbe19c912dc8872b7ad49fd2cfac5d068efb3581
juddus <github@juddus.com>
2020-07-04 16:26:43 +0100

    fix enhancements error

--- ava.config.js
+++ ava.config.js
@@ -2,7 +2,7 @@ const config = {
   files: ['tests/ava/**/*.ts'],
   sources: ['src/**/*.ts'],
   extensions: ['ts'],
-  compileEnhancements: false,
+  compileEnhancements: true,
   cache: true,
   require: ['ts-node/register'],
 };
