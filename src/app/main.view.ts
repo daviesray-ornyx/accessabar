50be6d9239a30ebe6d78569927c3b0ce035a68aa
juddus <github@juddus.com>
2020-07-16 23:46:58 +0100

    add settings to inner bar

--- src/app/main.view.ts
+++ src/app/main.view.ts
@@ -6,15 +6,35 @@ import ttsPrompt from './components/tts_prompt.component';
 import funcArea from './components/function_area.component';
 import settingsMenu from './components/settings.component';
 import aboutMenu from './components/about.component';
+import {switchEl} from './components/menus.component';
+import {aceSpeakTooltipsToggle} from './actions/ace.actions';
+
+const innerBarSettings = state => {
+  return [
+    h(
+      'ab-inner-bar-settings',
+      {class: 'ab-inner-bar-settings'},
+      switchEl(
+        state.aceSpeakTooltips,
+        aceSpeakTooltipsToggle,
+        'Speak Tooltips',
+        'Read tooltips aloud on hover'
+      )
+    ),
+  ];
+};
 
 const innerBar = state => {
   return h('ab-inner-bar', {class: 'ab-bar ab-growable'}, [
     h('ab-logo', {class: 'ab-logo', 'aria-label': 'Ace logo'}, [
       h('ab-logo-img', {class: 'ab-logo-img', alt: 'Ace Logo'}),
     ]),
-    h('ab-button-area-container', {class: 'ab-bar-container ab-growable'}, [
-      buttonArea(state),
-    ]),
+    h(
+      'ab-button-area-container',
+      {class: 'ab-button-area-container ab-bar-container ab-growable'},
+      [buttonArea(state)]
+    ),
+    innerBarSettings(state),
   ]);
 };
 
