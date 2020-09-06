d286345f66e22c3a33452bc212455896eb9dfaff
daviesray-ornyx <daviesray-ornyx@users.noreply.github.com>
2020-09-06 08:07:27 +0300

    Update src/app/subscriptions/keyboard_shortcuts.subscriptions.ts
    Co-authored-by: Elliott Judd <github@juddus.com>

--- src/app/subscriptions/keyboard_shortcuts.subscriptions.ts
+++ src/app/subscriptions/keyboard_shortcuts.subscriptions.ts
@@ -3,7 +3,7 @@ import buildKeyCombination from '../actions/shortcuts.actions';
 // KeyDown Subscription
 const subKeyDown = (dispatch, props) => {
   const hander = event => {
-    dispatch([props.action, event]);
+    dispatch(props.action, event);
   };
 
   window.addEventListener('keydown', hander);
